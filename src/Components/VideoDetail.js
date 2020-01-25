38140b45b382e921977acb33c4e97a68ba4b68c3
stevannn <sstojanovic@transperfect.com>
2020-01-25 23:11:02 +0100

    update masterssssss

--- src/Components/VideoDetail.js
+++ src/Components/VideoDetail.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const VideoDetail = (props) => {
-
-  return (
-    <div>Trt</div>
-  )
+const VideoDetail = ({video}) => {
+  if(!video) {
+    return <div>Loading...</div>;
+  }
+  return <div>{video.snippet.title}</div>;
 };
 
 export default VideoDetail;
